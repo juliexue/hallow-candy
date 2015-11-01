aae14fdfa53f7df431f56bf027c318c86fb38304
Ellen Wang <ellenwang95@yahoo.ca>
2015-11-01 00:45:46 -0400

    Locations stuffs - update location periodically

--- app/src/main/java/com/example/android/hallowcandy/HallowCandyActivity.java
+++ app/src/main/java/com/example/android/hallowcandy/HallowCandyActivity.java
@@ -2,6 +2,7 @@ package com.example.android.hallowcandy;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -42,16 +43,35 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationListener;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 
 
 public class HallowCandyActivity extends Activity implements
-		ConnectionCallbacks, OnConnectionFailedListener {
+		ConnectionCallbacks, OnConnectionFailedListener, LocationListener {
 
 	protected static final String TAG = "HallowCandyActivity";
 
+
+	/**
+	 * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+	 */
+	public static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+
+	/**
+	 * The fastest rate for active location updates. Exact. Updates will never be more frequent
+	 * than this value.
+	 */
+	public static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+			UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+
+	// Keys for storing activity state in the Bundle.
+	protected final static String REQUESTING_LOCATION_UPDATES_KEY = "requesting-location-updates-key";
+	protected final static String LOCATION_KEY = "location-key";
+	protected final static String LAST_UPDATED_TIME_STRING_KEY = "last-updated-time-string-key";
 
 	/**
 	 * Provides the entry point to Google Play services.
@@ -59,12 +79,32 @@ public class HallowCandyActivity extends Activity implements
 	protected GoogleApiClient mGoogleApiClient;
 
 	/**
+	 * Stores parameters for requests to the FusedLocationProviderApi.
+	 */
+	protected LocationRequest mLocationRequest;
+
+	/**
 	 * Represents a geographical location.
 	 */
-	protected Location mLastLocation;
+	protected Location mCurrentLocation;
 
-	protected TextView mLatitudeText;
-	protected TextView mLongitudeText;
+	// UI Widgets.
+//	protected Button mStartUpdatesButton;
+//	protected Button mStopUpdatesButton;
+//	protected TextView mLastUpdateTimeTextView;
+	protected TextView mLatitudeTextView;
+	protected TextView mLongitudeTextView;
+
+	/**
+	 * Tracks the status of the location updates request. Value changes when the user presses the
+	 * Start Updates and Stop Updates buttons.
+	 */
+	protected Boolean mRequestingLocationUpdates;
+
+	/**
+	 * Time when the location was updated represented as a String.
+	 */
+	protected String mLastUpdateTime;
 
 	private static final int ACTION_TAKE_PHOTO_B = 1;
 
@@ -203,7 +243,8 @@ public class HallowCandyActivity extends Activity implements
 
 		if (mCurrentPhotoPath != null) {
 			setPic();
-			uploadPic();
+			updateUI();
+			//uploadPic();
 			galleryAddPic();
 			mCurrentPhotoPath = null;
 		}
@@ -251,9 +292,17 @@ public class HallowCandyActivity extends Activity implements
 		setContentView(R.layout.main);
 
 		//location stuff
-		mLatitudeText = (TextView) findViewById((R.id.textLatitude));
-		mLongitudeText = (TextView) findViewById((R.id.textLongitude));
+		mLatitudeTextView = (TextView) findViewById((R.id.textLatitude));
+		mLongitudeTextView = (TextView) findViewById((R.id.textLongitude));
 
+		mRequestingLocationUpdates = false;
+		mLastUpdateTime = "";
+
+		// Update values using data stored in the Bundle.
+		updateValuesFromBundle(savedInstanceState);
+
+		// Kick off the process of building a GoogleApiClient and requesting the LocationServices
+		// API.
 		buildGoogleApiClient();
 
 		//picture stuff
@@ -262,8 +311,8 @@ public class HallowCandyActivity extends Activity implements
 		mImageBitmap = null;
 
 		Button picBtn = (Button) findViewById(R.id.btnPicture);
-		setBtnListenerOrDisable( 
-				picBtn, 
+		setBtnListenerOrDisable(
+				picBtn,
 				mTakePicOnClickListener,
 				MediaStore.ACTION_IMAGE_CAPTURE
 		);
@@ -276,15 +325,138 @@ public class HallowCandyActivity extends Activity implements
 	}
 
 	/**
-	 * Builds a GoogleApiClient. Uses the addApi() method to request the LocationServices API.
+	 * Updates fields based on data stored in the bundle.
+	 *
+	 * @param savedInstanceState The activity state saved in the Bundle.
+	 */
+	private void updateValuesFromBundle(Bundle savedInstanceState) {
+		Log.i(TAG, "Updating values from bundle");
+		if (savedInstanceState != null) {
+			// Update the value of mRequestingLocationUpdates from the Bundle, and make sure that
+			// the Start Updates and Stop Updates buttons are correctly enabled or disabled.
+			if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+				mRequestingLocationUpdates = savedInstanceState.getBoolean(
+						REQUESTING_LOCATION_UPDATES_KEY);
+			}
+
+			// Update the value of mCurrentLocation from the Bundle and update the UI to show the
+			// correct latitude and longitude.
+			if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+				// Since LOCATION_KEY was found in the Bundle, we can be sure that mCurrentLocation
+				// is not null.
+				mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+			}
+
+			// Update the value of mLastUpdateTime from the Bundle and update the UI.
+			if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+				mLastUpdateTime = savedInstanceState.getString(LAST_UPDATED_TIME_STRING_KEY);
+			}
+			updateUI();
+		}
+	}
+
+	/**
+	 * Builds a GoogleApiClient. Uses the {@code #addApi} method to request the
+	 * LocationServices API.
 	 */
 	protected synchronized void buildGoogleApiClient() {
+		Log.i(TAG, "Building GoogleApiClient");
 		mGoogleApiClient = new GoogleApiClient.Builder(this)
 				.addConnectionCallbacks(this)
 				.addOnConnectionFailedListener(this)
 				.addApi(LocationServices.API)
 				.build();
+		createLocationRequest();
 	}
+
+
+	/**
+	 * Sets up the location request. Android has two location request settings:
+	 * {@code ACCESS_COARSE_LOCATION} and {@code ACCESS_FINE_LOCATION}. These settings control
+	 * the accuracy of the current location. This sample uses ACCESS_FINE_LOCATION, as defined in
+	 * the AndroidManifest.xml.
+	 * <p/>
+	 * When the ACCESS_FINE_LOCATION setting is specified, combined with a fast update
+	 * interval (5 seconds), the Fused Location Provider API returns location updates that are
+	 * accurate to within a few feet.
+	 * <p/>
+	 * These settings are appropriate for mapping applications that show real-time location
+	 * updates.
+	 */
+	protected void createLocationRequest() {
+		mLocationRequest = new LocationRequest();
+
+		// Sets the desired interval for active location updates. This interval is
+		// inexact. You may not receive updates at all if no location sources are available, or
+		// you may receive them slower than requested. You may also receive updates faster than
+		// requested if other applications are requesting location at a faster interval.
+		mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		// Sets the fastest rate for active location updates. This interval is exact, and your
+		// application will never receive updates faster than this value.
+		mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+		mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+	}
+
+	/**
+	 * Handles the Start Updates button and requests start of location updates. Does nothing if
+	 * updates have already been requested.
+	 */
+	public void startUpdatesButtonHandler() {
+		if (!mRequestingLocationUpdates) {
+			mRequestingLocationUpdates = true;
+			startLocationUpdates();
+		}
+	}
+
+	/**
+	 * Handles the Stop Updates button, and requests removal of location updates. Does nothing if
+	 * updates were not previously requested.
+	 */
+	public void stopUpdatesButtonHandler() {
+		if (mRequestingLocationUpdates) {
+			mRequestingLocationUpdates = false;
+			stopLocationUpdates();
+		}
+	}
+
+	/**
+	 * Requests location updates from the FusedLocationApi.
+	 */
+	protected void startLocationUpdates() {
+		// The final argument to {@code requestLocationUpdates()} is a LocationListener
+		// (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+		LocationServices.FusedLocationApi.requestLocationUpdates(
+				mGoogleApiClient, mLocationRequest, this);
+	}
+
+
+	/**
+	 * Updates the latitude, the longitude, and the last location time in the UI.
+	 */
+	private void updateUI() {
+		mLatitudeTextView.setText(String.format("%s: %f", "Latitude:",
+				mCurrentLocation.getLatitude()));
+		mLongitudeTextView.setText(String.format("%s: %f", "Longitude:",
+				mCurrentLocation.getLongitude()));
+
+
+	}
+
+	/**
+	 * Removes location updates from the FusedLocationApi.
+	 */
+	protected void stopLocationUpdates() {
+		// It is a good practice to remove location requests when the activity is in a paused or
+		// stopped state. Doing so helps battery performance and is especially
+		// recommended in applications that request frequent location updates.
+
+		// The final argument to {@code requestLocationUpdates()} is a LocationListener
+		// (http://developer.android.com/reference/com/google/android/gms/location/LocationListener.html).
+		LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+	}
+
 
 	@Override
 	protected void onStart() {
@@ -292,12 +464,33 @@ public class HallowCandyActivity extends Activity implements
 		mGoogleApiClient.connect();
 	}
 
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// Within {@code onPause()}, we pause location updates, but leave the
+		// connection to GoogleApiClient intact.  Here, we resume receiving
+		// location updates if the user has requested them.
+
+		if (mGoogleApiClient.isConnected() && mRequestingLocationUpdates) {
+			startLocationUpdates();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// Stop location updates to save battery, but don't disconnect the GoogleApiClient object.
+		if (mGoogleApiClient.isConnected()) {
+			stopLocationUpdates();
+		}
+	}
+
 	@Override
 	protected void onStop() {
+		mGoogleApiClient.disconnect();
+
 		super.onStop();
-		if (mGoogleApiClient.isConnected()) {
-			mGoogleApiClient.disconnect();
-		}
 	}
 
 	@Override
@@ -316,7 +509,14 @@ public class HallowCandyActivity extends Activity implements
 	@Override
 	protected void onSaveInstanceState(Bundle outState) {
 		outState.putParcelable(BITMAP_STORAGE_KEY, mImageBitmap);
-		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null) );
+		outState.putBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY, (mImageBitmap != null));
+
+		//save location details
+//		outState.putDouble(LOCATION_LATITUDE, mLastLocation.getLatitude());
+//		outState.putDouble(LOCATION_LONGITUDE, mLastLocation.getLongitude());
+
+		outState.putParcelable(LOCATION_KEY, mCurrentLocation);
+
 		super.onSaveInstanceState(outState);
 	}
 
@@ -329,6 +529,10 @@ public class HallowCandyActivity extends Activity implements
 				savedInstanceState.getBoolean(IMAGEVIEW_VISIBILITY_STORAGE_KEY) ?
 						ImageView.VISIBLE : ImageView.INVISIBLE
 		);
+
+		mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+//		mLastLocation.setLatitude(savedInstanceState.getParcelable(LOCATION_KEY));
+//		mLastLocation.setLongitude(savedInstanceState.getDouble(LOCATION_LONGITUDE));
 	}
 
 	/**
@@ -372,28 +576,44 @@ public class HallowCandyActivity extends Activity implements
 	 */
 	@Override
 	public void onConnected(Bundle connectionHint) {
-		// Provides a simple way of getting a device's location and is well suited for
-		// applications that do not require a fine-grained location and that do not need location
-		// updates. Gets the best and most recent location currently available, which may be null
-		// in rare cases when a location is not available.
-		mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-		if (mLastLocation != null) {
-			mLatitudeText.setText(String.format("%s: %f", "Latitude:",
-					mLastLocation.getLatitude()));
-			mLongitudeText.setText(String.format("%s: %f", "Longitude:",
-					mLastLocation.getLongitude()));
-		} else {
-			Toast.makeText(this, "No location detected. :(", Toast.LENGTH_LONG).show();
+		Log.i(TAG, "Connected to GoogleApiClient");
+
+		// If the initial location was never previously requested, we use
+		// FusedLocationApi.getLastLocation() to get it. If it was previously requested, we store
+		// its value in the Bundle and check for it in onCreate(). We
+		// do not request it again unless the user specifically requests location updates by pressing
+		// the Start Updates button.
+		//
+		// Because we cache the value of the initial location in the Bundle, it means that if the
+		// user launches the activity,
+		// moves to a new location, and then changes the device orientation, the original location
+		// is displayed as the activity is re-created.
+		if (mCurrentLocation == null) {
+			mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+			mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+			updateUI();
+		}
+
+		// If the user presses the Start Updates button before GoogleApiClient connects, we set
+		// mRequestingLocationUpdates to true (see startUpdatesButtonHandler()). Here, we check
+		// the value of mRequestingLocationUpdates and if it is true, we start location updates.
+		mRequestingLocationUpdates = true;
+		if (mRequestingLocationUpdates) {
+			startLocationUpdates();
 		}
 	}
 
+	/**
+	 * Callback that fires when the location changes.
+	 */
 	@Override
-	public void onConnectionFailed(ConnectionResult result) {
-		// Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-		// onConnectionFailed.
-		Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+	public void onLocationChanged(Location location) {
+		mCurrentLocation = location;
+		mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+		updateUI();
+		Toast.makeText(this, "location updated!!",
+				Toast.LENGTH_SHORT).show();
 	}
-
 
 	@Override
 	public void onConnectionSuspended(int cause) {
@@ -401,6 +621,13 @@ public class HallowCandyActivity extends Activity implements
 		// attempt to re-establish the connection.
 		Log.i(TAG, "Connection suspended");
 		mGoogleApiClient.connect();
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		// Refer to the javadoc for ConnectionResult to see what error codes might be returned in
+		// onConnectionFailed.
+		Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
 	}
 
 }
